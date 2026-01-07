fc90178bb98df5eb6de5e6e6f092cb2a02733ad6
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-01-07 20:28:31 -0300

    feat: using todo plugin as middleware

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import App from "fusion-react";
 import React from "react";
 import Root from "./components/root";
 import HelmetPlugin from "fusion-plugin-react-helmet-async";
+import TodosPlugin from "./plugins/todos";
 
 export default async function start() {
   const app = new App(Root);
   app.register(HelmetPlugin);
+  if (__NODE__) {
+    app.middleware(TodosPlugin);
+  }
   return app;
 }
