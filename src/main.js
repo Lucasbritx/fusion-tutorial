9afdfbc73cd7065b8e588047f132dbc2769c88e9
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2026-01-07 21:33:02 -0300

    feat: add koa-bodyparser

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import TodosPlugin from "./plugins/todos";
 export default async function start() {
   const app = new App(Root);
   app.register(HelmetPlugin);
+  app.middleware(require("koa-bodyparser")());
   if (__NODE__) {
     app.middleware(TodosPlugin);
   }
